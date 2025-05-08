ab5b12725f95d9f5c95b128dc634104e186bee7a
jmlee2147 <jm2147@khu.ac.kr>
2025-05-09 05:52:18 +0900

    initial setting

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+<div className="flex h-dvh w-full justify-center">
+      <div className="flex w-full max-w-[600px] flex-col bg-background">
+        <p className="w-6 h-6 bg-red-500 text-red">fadsfasd</p>
+      </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,93 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontSize: {
+        "title-01": [
+          "20px",
+          {
+            lineHeight: "140%",
+            letterSpacing: "-0.005em",
+          },
+        ],
+
+        "title-02": [
+          "18px",
+          {
+            lineHeight: "140%",
+            letterSpacing: "-0.005em",
+          },
+        ],
+
+        "body-01": [
+          "16px",
+          {
+            lineHeight: "150%",
+            letterSpacing: "0em",
+          },
+        ],
+
+        "body-02": [
+          "14px",
+          {
+            lineHeight: "150%",
+            letterSpacing: "0em",
+          },
+        ],
+
+        "body-03": [
+          "12px",
+          {
+            lineHeight: "150%",
+            letterSpacing: "0em",
+          },
+        ],
+
+        "caption-01": [
+          "10px",
+          {
+            lineHeight: "140%",
+            letterSpacing: "0.2px",
+          },
+        ],
+
+        "caption-02": [
+          "8px",
+          {
+            lineHeight: "140%",
+            letterSpacing: "0.2px",
+          },
+        ],
+      },
+
+      fontWeight: {
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+      },
+
+      colors: {
+        black: "#121213",
+        white: "#FDFDFD",
+        gray: {
+          100: "#F4F6F8",
+          200: "#D8DCE2",
+          300: "#D8DCE2",
+          400: "#C4CAD4",
+          500: "#A5ADBD",
+        },
+        green: {
+          300: "#A8D881",
+          500: "#7AAD51",
+        },
+        red: {
+          300: "#F6A6A6",
+          500: "#F16C6C",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
